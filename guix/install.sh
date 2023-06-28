78fc7d40f744f851793e2421bd0758ea1ac5e7e4
cygauss <117000961+cygauss@users.noreply.github.com>
2023-06-28 03:35:14 -0500

    Add files via upload

---
+++ guix/install.sh
@@ -0,0 +1,11 @@
+mkfs.fat -F32 /dev/nvme0n1p1
+mkfs.ext4 /dev/nvme0n1p2
+mount /dev/nvme0n1p2 /mnt
+mkdir /mnt/boot
+mount /dev/nvme0n1p1 /mnt/boot
+herd start cow-store /mnt
+mkdir /mnt/etc
+cd /mnt/etc
+wget raw.githubusercontent.com/cygauss/guixy/main/config.scm
+cd -
+guix system init /mnt/etc/config.scm /mnt --substitute-urls='https://mirror.sjtu.edu.cn/guix/'
