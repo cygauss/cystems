137234eb27be82eda9a933a48a9f7b9f9965de85
cygauss <117000961+cygauss@users.noreply.github.com>
2023-06-29 00:12:34 +0800

    Update 3.sh

--- cygen/install.sh/3.sh
+++ cygen/install.sh/3.sh
@@ -1,19 +1,15 @@
-emerge linux-firmware intel-microcode gentoo-sources genkernel
+echo "sys-kernel/git-sources ~amd64" > /etc/portage/package.accept_keywords/00-manual
+emerge linux-firmware git-sources
 eselect kernel set 1
-genkernel --mountboot --install all
+cd /usr/src/linux*
+make -j24 && make modules_install
+make install
+cd -
 echo -e "/dev/nvme0n1p1 /boot vfat defaults,noatime 0 2\n/dev/nvme0n1p2 / ext4 noatime 0 1" >> /etc/fstab
 echo cygen > /etc/hostname
-emerge app-admin/sysklogd sys-process/cronie net-misc/chrony net-misc/dhcpcd
+emerge app-admin/sysklogd sys-process/cronie net-misc/chrony net-misc/dhcpcd 
 rc-update add dhcpcd default
 rc-update add sysklogd default
 rc-update add cronie default
 rc-update add chronyd default
-echo -e "sys-kernel/git-sources ~amd64\nsys-boot/systemd-boot ~amd64" > /etc/portage/package.accept_keywords/00-manual
-echo "sys-apps/systemd-utils boot" > /etc/portage/package.use/00-manual
-emerge systemd-boot
-bootctl --esp-path=/boot install
-cd /boot
-echo -e "title Gentoo\nlinux /$(ls vmlinuz*)\ninitrd /$(ls ini*)\noptions root=PARTUUID= quiet $(lsblk -o name,partuuid)" > /boot/loader/entries/gentoo.conf
-nano /boot/loader/entries/gentoo.conf
 passwd
-cd -
