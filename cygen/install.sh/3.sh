96590138b1564f6dfc6178f091577a568406453d
cygauss <117000961+cygauss@users.noreply.github.com>
2023-06-29 00:19:01 +0800

    Update 3.sh

--- cygen/install.sh/3.sh
+++ cygen/install.sh/3.sh
@@ -1,15 +1,20 @@
 echo "sys-kernel/git-sources ~amd64" > /etc/portage/package.accept_keywords/00-manual
 emerge linux-firmware git-sources
 eselect kernel set 1
+passwd
 cd /usr/src/linux*
+
+make menuconfig
 make -j24 && make modules_install
 make install
 cd -
 echo -e "/dev/nvme0n1p1 /boot vfat defaults,noatime 0 2\n/dev/nvme0n1p2 / ext4 noatime 0 1" >> /etc/fstab
 echo cygen > /etc/hostname
-emerge app-admin/sysklogd sys-process/cronie net-misc/chrony net-misc/dhcpcd 
+emerge app-admin/sysklogd sys-process/cronie net-misc/chrony net-misc/dhcpcd sys-boot/grub sys-boot/os-prober
 rc-update add dhcpcd default
 rc-update add sysklogd default
 rc-update add cronie default
 rc-update add chronyd default
-passwd
+
+grub-install /boot/efi --removable
+grub-mkconfig -o /boot/grub/grub.cfg
