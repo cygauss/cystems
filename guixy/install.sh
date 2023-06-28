ccdedd6289d3e3fe3a111ed7ff98f67952484a1b
cygauss <117000961+cygauss@users.noreply.github.com>
2023-06-28 16:56:46 +0800

    Update install.sh

--- guixy/install.sh
+++ guixy/install.sh
@@ -6,6 +6,6 @@ mount /dev/nvme0n1p1 /mnt/boot
 herd start cow-store /mnt
 mkdir /mnt/etc
 cd /mnt/etc
-wget raw.githubusercontent.com/cygauss/guixy/main/config.scm
+wget raw.githubusercontent.com/cygauss/awesome-linuxy/main/guixy/config.scm
 cd -
 guix system init /mnt/etc/config.scm /mnt --substitute-urls='https://mirror.sjtu.edu.cn/guix/'
