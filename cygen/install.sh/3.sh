8b4c9badf7981edfb0bf09b03395a65a90b735e6
cygauss <117000961+cygauss@users.noreply.github.com>
2023-06-29 00:20:31 +0800

    Update 3.sh

--- cygen/install.sh/3.sh
+++ cygen/install.sh/3.sh
@@ -3,18 +3,17 @@ emerge linux-firmware git-sources
 eselect kernel set 1
 passwd
 cd /usr/src/linux*
-
+wget raw.githubusercontent.com/cygauss/awesome-linuxy/main/kernelconfig/ori.config
 make menuconfig
 make -j24 && make modules_install
 make install
 cd -
-echo -e "/dev/nvme0n1p1 /boot vfat defaults,noatime 0 2\n/dev/nvme0n1p2 / ext4 noatime 0 1" >> /etc/fstab
+echo -e "/dev/nvme2n1p1 /boot/efi vfat defaults,noatime 0 2\n/dev/nvme2n1p2 / ext4 noatime 0 1" >> /etc/fstab
 echo cygen > /etc/hostname
 emerge app-admin/sysklogd sys-process/cronie net-misc/chrony net-misc/dhcpcd sys-boot/grub sys-boot/os-prober
 rc-update add dhcpcd default
 rc-update add sysklogd default
 rc-update add cronie default
 rc-update add chronyd default
-
 grub-install /boot/efi --removable
 grub-mkconfig -o /boot/grub/grub.cfg
