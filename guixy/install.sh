200579b9b7f81e78cf14635ff16da6f853f79b78
cygauss <117000961+cygauss@users.noreply.github.com>
2023-06-28 20:54:08 +0800

    Update install.sh

--- guixy/install.sh
+++ guixy/install.sh
@@ -1,9 +1,9 @@
-mkfs.fat -F32 /dev/nvme2n1p1
-mkfs.ext4 /dev/nvme2n1p3
-mount /dev/nvme2n1p3 /mnt
+mkfs.fat -F32 /dev/nvme3n1p1
+mkfs.ext4 /dev/nvme3n1p2
+mount /dev/nvme3n1p2 /mnt
 mkdir /mnt/boot
 mkdir /mnt/boot/efi
-mount /dev/nvme2n1p1 /mnt/boot/efi
+mount /dev/nvme3n1p1 /mnt/boot/efi
 herd start cow-store /mnt
 mkdir /mnt/etc
 cd /mnt/etc
