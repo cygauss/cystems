c46aedc5e2989579f7aeee8189e7e30105752b39
cygauss <117000961+cygauss@users.noreply.github.com>
2023-06-29 22:34:00 +0800

    Update 1.sh

--- cygen/install.sh/1.sh
+++ cygen/install.sh/1.sh
@@ -7,7 +7,7 @@ wget raw.githubusercontent.com/cygauss/awesome-linuxy/main/cygen/install.sh/2.sh
 wget raw.githubusercontent.com/cygauss/awesome-linuxy/main/cygen/install.sh/3.sh
 chronyd -q
 wget https://mirrors.ustc.edu.cn/gentoo/releases/amd64/autobuilds/20230625T165009Z/stage3-amd64-openrc-20230625T165009Z.tar.xz
-tar xpvf stage*
+tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 cd -
 rm /mnt/gentoo/etc/portage/make.conf
 cd /mnt/gentoo/etc/portage/
