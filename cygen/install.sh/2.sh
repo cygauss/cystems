9e2a96959645756b6402b5778b3c0affde6b24e4
cygauss <117000961+cygauss@users.noreply.github.com>
2023-06-30 02:15:10 +0800

    Update 2.sh

--- cygen/install.sh/2.sh
+++ cygen/install.sh/2.sh
@@ -1,5 +1,7 @@
 mkdir /boot/efi
-#mount /dev/nvme2n1p1 /boot/efi
+#echo -e "/dev/nvme2n1p1 /boot/efi vfat defaults,noatime 0 2\n/dev/nvme2n1p2 / ext4 noatime 0 1\ntmpfs /tmp tmpfs size=128G,mode=775 0 0" >> /etc/fstab
+mount /boot/efi
+mount /tmp
 emerge-webrsync
 emerge --update --deep --newuse @world
 echo "Asia/Chongqing" > /etc/timezone
