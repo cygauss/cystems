114eaf481b4493bcae57554eafde15aa05ddb786
cygauss <117000961+cygauss@users.noreply.github.com>
2023-07-03 17:10:06 +0800

    Update 1.sh

--- cygen/install.sh/1.sh
+++ cygen/install.sh/1.sh
@@ -1,8 +1,8 @@
 wget raw.githubusercontent.com/cygauss/awesome-linuxy/main/cygen/install.sh/4.sh
 #mkfs.fat -F 32 /dev/nvme2n1p1
 #mkfs.ext4 /dev/nvme2n1p2
-#echo -e "/dev/nvme2n1p1 /boot/efi vfat defaults,noatime 0 2\n/dev/nvme2n1p2 / ext4 noatime 0 1\ntmpfs /tmp tmpfs size=128G,mode=775 0 0" >> /etc/fstab
-mount /mnt/gentoo
+mkdir /mnt/gentoo
+#mount /dev/nvme2n1p2 /mnt/gentoo
 cd /mnt/gentoo
 wget raw.githubusercontent.com/cygauss/awesome-linuxy/main/cygen/install.sh/2.sh
 wget raw.githubusercontent.com/cygauss/awesome-linuxy/main/cygen/install.sh/3.sh
@@ -10,6 +10,7 @@ chronyd -q
 #wget http://mirrors.ustc.edu.cn/gentoo/releases/amd64/autobuilds/20230625T165009Z/stage3-amd64-openrc-20230625T165009Z.tar.xz
 tar xpvf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner
 cd -
+#echo -e "/dev/nvme2n1p1 /boot/efi vfat defaults,noatime 0 2\n/dev/nvme2n1p2 / ext4 noatime 0 1\ntmpfs /tmp tmpfs size=128G,mode=775 0 0" >> /mnt/etc/fstab
 rm /mnt/gentoo/etc/portage/make.conf
 cd /mnt/gentoo/etc/portage/
 wget https://raw.githubusercontent.com/cygauss/awesome-linuxy/main/cygen/make.conf
@@ -17,7 +18,6 @@ cd -
 mkdir --parents /mnt/gentoo/etc/portage/repos.conf
 cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
-cp /etc/fstab /mnt/gentoo/etc/fstab
 mkdir /mnt/gentoo/boot/efi
 mount --types proc /proc /mnt/gentoo/proc
 mount --rbind /sys /mnt/gentoo/sys
