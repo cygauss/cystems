3243bd57fee4ad4de29b4d351176a06c642e2d16
cygauss <117000961+cygauss@users.noreply.github.com>
2023-11-22 18:28:58 +0800

    Update 1.sh

--- archy/install.sh/1.sh
+++ archy/install.sh/1.sh
@@ -8,3 +8,6 @@ mkdir /mnt/boot
 echo -e "Server = http://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch\nServer = http://mirrors.pku.edu.cn/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
 pacstrap -K /mnt base linux linux-firmware networkmanager efibootmgr intel-ucode
 genfstab -U /mnt >> /mnt/etc/fstab
+cd /mnt
+wget raw.githubusercontent.com/cygauss/cystems/main/archy/install.sh/2.sh
+cd -
