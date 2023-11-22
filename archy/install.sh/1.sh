8cc0781874c90aa2a81af88cbf56629b898f949c
cygauss <117000961+cygauss@users.noreply.github.com>
2023-11-22 19:22:47 +0800

    Update 1.sh

--- archy/install.sh/1.sh
+++ archy/install.sh/1.sh
@@ -5,9 +5,9 @@ timedatectl
 #mount /dev/nvme0n1p2 /mnt 
 mkdir /mnt/boot
 #mount /dev/nvme0n1p1 /mnt
-echo -e "Server = http://mirrors.tuna.tsinghua.edu.cn/archlinux/$repo/os/$arch\nServer = http://mirrors.pku.edu.cn/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
+echo -e "Server = http://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch\nServer = http://mirrors.pku.edu.cn/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
 pacstrap -K /mnt base linux linux-firmware networkmanager efibootmgr intel-ucode
 genfstab -U /mnt >> /mnt/etc/fstab
 cd /mnt
-wget raw.githubusercontent.com/cygauss/cystems/main/archy/install.sh/2.sh
+curl -O raw.githubusercontent.com/cygauss/cystems/main/archy/install.sh/2.sh
 cd -
