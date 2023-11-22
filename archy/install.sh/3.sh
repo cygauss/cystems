c84a42dfec4fa1f401e0b8f4fd224a7377ead518
cygauss <117000961+cygauss@users.noreply.github.com>
2023-11-22 20:41:15 +0800

    Create 3.sh

---
+++ archy/install.sh/3.sh
@@ -0,0 +1,11 @@
+##修改mirror和repo
+echo -e "Server = http://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch\nServer = http://mirrors.pku.edu.cn/archlinux/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
+echo -e "Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch\nServer = http://mirrors.pku.edu.cn/archlinuxcn/\$arch" > /etc/pacman.d/archlinuxcn-mirrorlist
+echo -e "[archlinuxcn]\nInclude = /etc/pacman.d/archlinuxcn-mirrorlist" >> /etc/pacman.conf
+##yay可以统筹aur和repos
+pacman -Sy archlinuxcn-keyring yay
+##用户配置
+pacman -S sudo nano
+useradd -m -G wheel cygauss
+passwd cygauss
+EDITOR="nano" visudo
