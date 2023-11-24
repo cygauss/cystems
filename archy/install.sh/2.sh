12041555b9d260683adf51ad985cbbde8106553e
cygauss <117000961+cygauss@users.noreply.github.com>
2023-11-24 18:17:18 +0800

    Update 2.sh

--- archy/install.sh/2.sh
+++ archy/install.sh/2.sh
@@ -3,9 +3,10 @@ echo -e "Server = http://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch
 echo -e "Server = http://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch\nServer = http://mirrors.pku.edu.cn/archlinuxcn/\$arch" > /etc/pacman.d/archlinuxcn-mirrorlist
 echo -e "[archlinuxcn]\nInclude = /etc/pacman.d/archlinuxcn-mirrorlist" >> /etc/pacman.conf
 ##yay可以统筹aur和repos
-pacman -Sy archlinuxcn-keyring yay
+pacman -Sy archlinuxcn-keyring
+pacman -S yay
 ##用户配置
-pacman -S sudo nano
+pacman -S sudo
 useradd -m -G wheel cygauss
 passwd cygauss
 EDITOR="nano" visudo
