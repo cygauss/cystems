3f4289fb92b0dd5143621b2bb8a57cd0ca41db6e
cygauss <117000961+cygauss@users.noreply.github.com>
2023-11-24 16:16:43 +0800

    Update 1-1.sh

--- archy/install.sh/1-1.sh
+++ archy/install.sh/1-1.sh
@@ -5,6 +5,10 @@ echo -e "en_SG.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_SG.UTF-8" >> /etc/locale.conf
 echo "Archy" >> /etc/hostname
-systemctl enable NetworkManager
+#systemctl enable NetworkManager
 passwd
+#efibootmgr -Bb 0000
+#efibootmgr -Bb 0001
+#efibootmgr -Bb 0002
+#efibootmgr -Bb 0003
 #efibootmgr -c -d /dev/nvme0n1p1 -l /vmlinuz-linux -L "Arch Linux" -u 'initrd=\intel-ucode.img initrd=\initramfs-linux.img root=/dev/nvme0n1p2 quiet rw nowatchdog'
