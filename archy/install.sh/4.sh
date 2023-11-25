fd8c2d2367459b78cb28f32abba3d2e0e35f336e
cygauss <117000961+cygauss@users.noreply.github.com>
2023-11-25 23:54:50 +0800

    Update 4.sh

--- archy/install.sh/4.sh
+++ archy/install.sh/4.sh
@@ -2,10 +2,11 @@
 ##GUIlib
 pacman -S gtk4 qt6-wayland qt5ct
 echo -e "QT_QPA_PLATFORM=wayland\nQT_QPA_PLATFORMTHEME=qt5ct" >> /etc/environment
-##Hyprland kitty
-pacman -S hyprland-nvidia kitty
+
+
 ##字体和输入法
 pacman -S noto-fonts-cjk fcitx5-im fcitx5-rime 
+
 ##修改环境变量和随hyprland启动,ibus项是因为kitty
 echo -e "GTK_IM_MODULE=fcitx\nQT_IM_MODULE=fcitx\nXMODIFIERS=@im=fcitx\nSDL_IM_MODULE=fcitx\nGLFW_IM_MODULE=ibus" >> /etc/environment
 fcitx5-configtool
