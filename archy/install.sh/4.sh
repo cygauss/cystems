e94fd67f8c8a9ef7a981ab85e10b03e7fb36afb1
cygauss <117000961+cygauss@users.noreply.github.com>
2023-11-25 16:43:13 +0800

    Update 4.sh

--- archy/install.sh/4.sh
+++ archy/install.sh/4.sh
@@ -3,7 +3,7 @@
 pacman -S gtk4 qt6-wayland qt5ct
 echo -e "QT_QPA_PLATFORM=wayland\nQT_QPA_PLATFORMTHEME=qt5ct" >> /etc/environment
 ##Hyprland kitty
-pacman -S hyprland-nvidia-hidpi-git kitty
+pacman -S hyprland-nvidia kitty
 ##字体和输入法
 pacman -S noto-fonts-cjk fcitx5-im fcitx5-rime 
 ##修改环境变量和随hyprland启动,ibus项是因为kitty
@@ -16,4 +16,4 @@ pacman -S pipewire wireplumber helvum
 
 ##v2raya
 pacman -S v2raya
-systemctl enable v2rayaervice
+systemctl enable v2raya.service
